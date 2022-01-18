8952627201a599a26ad3503bf0eedcf735b42786
gilmujjang <alsanrlf@naver.com>
2022-01-18 12:03:37 +0900

    [add]헤더 mobile 화면 작성

--- src/components/header.js
+++ src/components/header.js
@@ -122,7 +122,28 @@ function TabletHeader(){
 function MobileHeader(){
   return(
     <Head>
-      asdf
+      <div style={{margin: "0 36px"}}>
+        <div>
+          <Nav>
+            <span style={{marginLeft: "12px"}}>
+              <Hambuger alt="menu button" src="https://static.wanted.co.kr/images/icon-menu.png"></Hambuger>
+              <Wanted>wanted</Wanted>
+            </span>
+            <SignUp>회원가입하기</SignUp>
+          </Nav>
+        </div>
+        <div style={{display: "flex",alignItems: "center", justifyContent:"space-between"}}>
+          <div style={{display: "flex",alignItems: "center", margin: "8px 0"}}>
+            <li>홈</li>
+            <li>채용</li>
+            <li>이벤트</li>
+          </div>
+          <div style={{display: "flex"}}>
+            <li><svg xmlns="https://www.w3.org/2000/svg" xlink="https://www.w3.org/1999/xlink" width="18" height="18" viewBox="0 0 18 18"><defs><path id="qt2dnsql4a" d="M15.727 17.273a.563.563 0 10.796-.796l-4.875-4.875-.19-.165a.563.563 0 00-.764.028 5.063 5.063 0 111.261-2.068.562.562 0 101.073.338 6.188 6.188 0 10-1.943 2.894l4.642 4.644z"></path></defs><g fill="none" fill-rule="evenodd"><use fill="#333" fill-rule="nonzero" stroke="#333" stroke-width=".3" href="#qt2dnsql4a"></use></g></svg></li>
+            <li><svg width="18" height="18" xmlns="http://www.w3.org/2000/svg"><defs><path d="M9 7.5a1.5 1.5 0 1 1-.001 3.001A1.5 1.5 0 0 1 9 7.5zm5.05 0a1.5 1.5 0 1 1-.001 3.001A1.5 1.5 0 0 1 14.05 7.5zM4 7.5a1.5 1.5 0 1 1-.001 3.001A1.5 1.5 0 0 1 4 7.5z" id="a"></path></defs><g fill="none" fill-rule="evenodd"><mask id="b" fill="#fff"><use xlinkHref="#a"></use></mask><use fill="#333" xlinkHref="#a"></use><g mask="url(#b)" fill="#333"><path d="M0 0h18v18H0z"></path></g></g></svg></li>
+          </div>
+        </div>
+      </div>
     </Head>
   )
 }
