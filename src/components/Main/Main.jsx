6cb896fa2f85736b74b66f527f2d0e3109298a78
gilmujjang <alsanrlf@naver.com>
2022-02-24 15:20:09 +0900

    feat: 과제 데이터 추가

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,8 +1,20 @@
 import React from "react";
 import * as S from "./styled";
+import TaskData from "./TaskData";
 
 function Main() {
-  return <S.MainWrapper>과제한것들~</S.MainWrapper>;
+  return (
+    <S.MainWrapper>
+      {TaskData.map((task) => (
+        <>
+          <div>{task.company}</div>
+          <div>{task.repo}</div>
+          <div>{task.deploy}</div>
+          <div>{task.description}</div>
+        </>
+      ))}
+    </S.MainWrapper>
+  );
 }
 
 export default Main;
---
+++ src/components/Main/TaskData.js
@@ -0,0 +1,53 @@
+const TaskData = [
+  {
+    company: "와이어바알리/레드브릭",
+    repo: "https://github.com/pre-onboarding-course-team-6/1st-week-wirebarley-redbrick-task",
+    deploy: "https://pob-sixted-week1.herokuapp.com/",
+    description: "간단한 환율 계산기 구현",
+    date: "첫째주 월요일",
+    etc: ["REST api", "비동기"],
+  },
+  {
+    company: "설로인",
+    repo: "https://github.com/pre-onboarding-course-team-6/1st-week-sirloin-task",
+    deploy:
+      "https://pre-onboarding-course-team-6.github.io/1st-week-sirloin-task/",
+    description: "상품 정보 등록 페이지 구현",
+    date: "첫째 주 목요일",
+    etc: ["상태관리"],
+  },
+  {
+    company: "에이팀벤처스",
+    repo: "https://github.com/pre-onboarding-course-team-6/2nd-week-ateam-ventures",
+    deploy: "https://angry-albattani-8e8937.netlify.app/",
+    description: "카테고리별 상품 필터링 기능 구현",
+    date: "둘째 주 월요일",
+    etc: ["Typescript", "필터링"],
+  },
+  {
+    company: "Swit",
+    repo: "https://github.com/pre-onboarding-course-team-6/2nd-week-swit",
+    deploy: "https://infallible-panini-19ee47.netlify.app/",
+    description: "채팅 기능 구현",
+    date: "둘째 주 목요일",
+    etc: ["Redux"],
+  },
+  {
+    company: "더블엔씨",
+    repo: "https://github.com/pre-onboarding-course-team-6/3rd-week-ncnc",
+    deploy: "https://cocky-edison-1b96cb.netlify.app/",
+    description: "니콘내콘 클론코딩",
+    date: "셋째 주 월요일",
+    etc: ["Typescript", "Next.JS", "E2E테스트"],
+  },
+  {
+    company: "에너지밸런스",
+    repo: "https://github.com/pre-onboarding-course-team-6/3rd-week-energybalance",
+    deploy: "https://cocky-edison-1b96cb.netlify.app/",
+    description: "검색기능 구현",
+    date: "셋째 주 목요일",
+    etc: ["자동완성", "REST api", "비동기", "Typescript"],
+  },
+];
+
+export default TaskData;
